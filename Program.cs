a15b2fb144fcf25dae33982336230e2fd4672964
faaaami <faaaamil444@gmail.com>
2026-04-06 19:32:46 +0530

    add oop task-1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+        d.Name = "Tommy";
+        d.age = 5;
+        d.breed = "Labrador";
+
+        d.speak();
+
+        Cat c = new Cat();
+        c.Name = "Kitty";
+        c.age = 3;
+
+        c.speak();
+        c.meow();
+    }
+
+    class Animal
+    {
+        public string Name;
+        public int age;
+
+        public void speak()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {age}");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public string breed;
+    }
+
+    class Cat : Animal
+    {
+        public void meow()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+}
